099fe8ca42dba1e52c699f2ff32848db560a179b
Bystrov Daniil <bstrdn@gmail.com>
2020-11-24 20:42:20 +0300

    HW7_0.1_

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -2,17 +2,18 @@ package ru.javawebinar.topjava.web;
 
 import org.assertj.core.matcher.AssertionMatcher;
 import org.junit.jupiter.api.Test;
-import ru.javawebinar.topjava.UserTestData;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
 
 import java.util.List;
 
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.javawebinar.topjava.MealTestData.MEAL_MATCHER;
+import static ru.javawebinar.topjava.MealTestData.meals;
 import static ru.javawebinar.topjava.UserTestData.*;
-import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
 
 class RootControllerTest extends AbstractControllerTest {
 
@@ -36,19 +37,17 @@ class RootControllerTest extends AbstractControllerTest {
     @Test
     void getMeals() throws Exception {
         perform(get("/meals"))
-            .andDo(print())
-            .andExpect(status().isOk())
-            .andExpect(view().name("meals"))
-            .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
-            .andExpect(model().attribute("meals", hasSize(7)))
-//            .andExpect(model().attribute("meals", hasItem(
-//                    allOf(
-//                            hasProperty("id", is()),
-//                            hasProperty("name", is(UserTestData.user.getName()))
-//                    )
-//            )))
-        ;
-
-
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("meals"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+                .andExpect(model().attribute("meals", hasSize(7)))
+                .andExpect(model().attribute("meals",
+                        new AssertionMatcher<List<Meal>>() {
+                            @Override
+                            public void assertion(List<Meal> actual) throws AssertionError {
+                                MEAL_MATCHER.assertMatch(actual, meals);
+                            }
+                        }));
     }
 }
